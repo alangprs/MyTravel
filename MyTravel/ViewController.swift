60b31813510da4f9d067df6d2c27ca4810559638
will <will.weng@lig.com.tw>
2022-01-25 11:59:18 +0800

    新增 設定topBar文字

--- MyTravel/ViewController.swift
+++ MyTravel/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topBarView: TopBarView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        topBarViewSetup()
+    }
+    
+    func topBarViewSetup() {
+        topBarView.setTitle(title: "首頁")
     }
 
 
