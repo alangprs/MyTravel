121c5efd63180e28b75262cdd55ac4b6412ad71a
will <will.weng@lig.com.tw>
2022-01-25 16:40:01 +0800

    新增 networkAddress

---
+++ MyTravel/Model/NetworkStruct/NetworkAddress.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkAddress.swift
+//  MyTravel
+//
+//  Created by will on 2022/1/25.
+//
+
+import Foundation
+
+//MARK: - 景點
+let kAttractions: String = "https://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json"
