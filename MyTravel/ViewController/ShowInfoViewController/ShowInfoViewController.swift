591f0adcc5e5e34374485bc5548578618d8fdc6f
will <alangprs1@gmail.com>
2022-03-10 22:17:45 +0800

    新增 顯示titleCell 內容

--- MyTravel/ViewController/ShowInfoViewController/ShowInfoViewController.swift
+++ MyTravel/ViewController/ShowInfoViewController/ShowInfoViewController.swift
@@ -5,6 +5,10 @@
 //  Created by 翁燮羽 on 2022/3/8.
 // 展示點選到的景點內容
 
+enum CellSelect: Int, CaseIterable {
+    case image = 0, title = 1
+}
+
 import UIKit
 
 class ShowInfoViewController: UIViewController {
@@ -38,30 +42,50 @@ extension ShowInfoViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
-        let imageNib = UINib(nibName: "\(ImageCell.self)", bundle: nil)
-        tableView.register(imageNib, forCellReuseIdentifier: "\(ImageCell.self)")
+        tableView.creatCell(nibName: "\(ImageCell.self)")
+        tableView.creatCell(nibName: "\(TitleCell.self)")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return CellSelect.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let imageCell = tableView.dequeueReusableCell(withIdentifier: "\(ImageCell.self)") as? ImageCell else {
-            print("\(ShowInfoViewController.self) get imageCell fail")
-            return UITableViewCell()
+        
+        guard let cellItem = CellSelect(rawValue: indexPath.row) else {return UITableViewCell()}
+        
+        switch cellItem {
+        case .image:
+            
+            guard let imageCell = tableView.dequeueReusableCell(withIdentifier: "\(ImageCell.self)") as? ImageCell else {
+                print("\(ShowInfoViewController.self) get imageCell fail")
+                return UITableViewCell()
+            }
+            
+            if let imageUrl = areaData?.picture1 {
+                imageCell.convertCell(data: imageUrl)
+            } else {
+                print("\(ShowInfoViewController.self) get imageUrl fail")
+            }
+            
+            return imageCell
+            
+        case .title:
+            guard let titleCell = tableView.dequeueReusableCell(withIdentifier: "\(TitleCell.self)") as? TitleCell else {
+                print("\(ShowInfoViewController.self) get titleCell fail")
+                return UITableViewCell()
+            }
+            
+            if let title = areaData?.name {
+                titleCell.convertCell(title: title)
+            } else {
+                print("\(ShowInfoViewController.self) get title fail")
+            }
+            
+            return titleCell
         }
         
-        if let imageUrl = areaData?.picture1 {
-            imageCell.convertCell(data: imageUrl)
-        } else {
-            print("\(ShowInfoViewController.self) get imageUrl fail")
-        }
-        
-        return imageCell
     }
-    
-    
 }
