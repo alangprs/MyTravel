3e3fd6cb065bf70bda01e66b203ee0e8d1e029b9
will <will.weng@lig.com.tw>
2022-04-13 14:04:06 +0800

    修改 首頁陰影效果

--- MyTravel/View/ViewControllerCell/ViewControllerCell.swift
+++ MyTravel/View/ViewControllerCell/ViewControllerCell.swift
@@ -11,28 +11,28 @@ class ViewControllerCell: UICollectionViewCell {
 
     
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var backgroundColorView: UIView!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var shadowView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         //賦予圓角
         containerView.layer.cornerRadius = 15
-        backgroundColorView.layer.cornerRadius = 15
         photoImageView.layer.cornerRadius = 15
         
+        
         //設定陰影位置
-        containerView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        shadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
         //陰影透明度
-        containerView.layer.shadowOpacity = 1
-        //隱影範圍
-        containerView.layer.shadowRadius = 5
-        containerView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOpacity = 0.5
+        //陰影範圍
+        shadowView.layer.shadowRadius = 5
+        shadowView.layer.shadowColor = UIColor(named: "D26900-40")?.cgColor
         
-        photoImageView.clipsToBounds = true
-        
+        containerView.clipsToBounds = true
+        shadowView.layer.cornerRadius = 15
     }
     
     ///給予cell資料
