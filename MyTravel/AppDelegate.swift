a26552a88b4c68e1228b7df611f6817a115cce06
Will_Weng <Will_Weng@cmoney.com.tw>
2022-11-26 23:32:01 +0800

    add - 創建 景點/縣市 表格

--- MyTravel/AppDelegate.swift
+++ MyTravel/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        /// 實體化 db
+        _ = FMDBManager.shart
         return true
     }
 
---
+++ MyTravel/Model/FMDBManager/RegionTable.swift
@@ -0,0 +1,12 @@
+//
+//  RegionTable.swift
+//  MyTravel
+//
+//  Created by 翁燮羽 on 2022/11/26.
+//
+
+import Foundation
+
+class RegionTable {
+    static let region = "region"
+}
